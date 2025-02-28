0941815bb7168d5ad561283aa8cef24bcf2f2a3a
greenJaa <greenzmey@gmail.com>
2025-02-28 22:35:30 +0200

    Adding script file to repository

---
+++ post_install_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#########################################
+#Develop by: Oleg Ischouk aka Silent-Mobius
+#Purpose:
+#Date: 28/02/2025
+#Version: 1.0.0
+set -o errexit
+set -o pipefail
+########################################
+
+
+echo "First script"
