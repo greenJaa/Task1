378668cfe35d6457bb2a4133986ee0217506eebe
Oleg <greenzmey@gmail.com>
2025-03-01 01:10:00 +0200

    Install git with apt-get

--- post_install_script.sh
+++ post_install_script.sh
@@ -5,10 +5,10 @@
 #Date: 28/02/2025
 #Version: 1.0.0
 set -o errexit
-set -o pipefail
+#set -o pipefail
 ########################################
 
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg     | gpg --dearmor     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+   24  echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main'     | sudo tee /etc/apt/sources.list.d/vscodium.list
+   25  sudo apt update && sudo apt install codium
 
-echo "First script"
-
-echo "1 edit script"
