23531897227311aee8a0a1cdb6f244a8fc04856b
Oleg <greenzmey@gmail.com>
2025-03-05 17:56:17 +0200

    commit :

--- post_install_script.sh
+++ post_install_script.sh
@@ -8,7 +8,5 @@ set -o errexit
 #set -o pipefail
 ########################################
 
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg     | gpg --dearmor     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-   24  echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main'     | sudo tee /etc/apt/sources.list.d/vscodium.list
-   25  sudo apt update && sudo apt install codium
+     sudo apt update && sudo apt install codium
 
