029ca4fb433d663c61ac1fc24629d0a3efcc5b89
zmey <greenzmey@gmail.com>
2025-03-08 00:26:57 +0200

    14

--- post_install_script.sh
+++ post_install_script.sh
@@ -8,7 +8,16 @@ set -o errexit
 #set -o pipefail
 ########################################
 
-     sudo apt update && sudo apt install codium
+
+if dpkg -s codium &>/dev/null; then
+  echo 'dcodium is installed'
+
+else {
+    sudo wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg -O /usr/share/keyrings/vscodium-archive-keyring.asc
+    echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+    sudo apt update && sudo apt install codium
+}
+fi
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #echo -e "call plug#begin()\nPlug 'preservim/NERDTree'\ncall plug#end()"> ~/.vimrc
